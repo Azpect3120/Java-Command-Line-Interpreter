a806ea828898dd99c244d7da2f00f4813d2e8b40
Hayden Hargreaves <hhargr836@west-mec.org>
2023-05-23 16:31:45 -0700

    createWindow() method added

--- cmd.java
+++ cmd.java
@@ -1,7 +1,47 @@
 package CommandLineInterpreter;
 
+import java.lang.reflect.Constructor;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class cmd {
     public static void main (String[] args) {
+
+        Window window = new Window();
+
+        window.createWindow();
+
+    }
+
+
+    static class Window {
+        // Window size
+        int height = 960;
+        int width = 540;
+
+
+        // Create the window
+        void createWindow () {
+            // Instantiate window
+            JFrame window = new JFrame();
+
+            // Window title & size
+            window.setVisible(true);
+            window.setTitle("Jarvis: Command Line");
+            window.setSize(this.height, this.width);
+
+            // Defines the 'x' close event on the window
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
+
+
+    }
+
+
+    static class Commands {
+
+
 
     }
 
