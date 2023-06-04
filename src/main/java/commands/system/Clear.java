349fb753e8a020bee7992b28811abfac1029321f
Azpect3120 <hhargreaves2006@gmail.com>
2023-06-04 14:20:58 -0700

    clear command was migrated! THIS DOES NOT WORK IN THE INTELLIJ CONSOLE!

--- src/main/java/commands/system/Clear.java
+++ src/main/java/commands/system/Clear.java
@@ -1,4 +1,21 @@
 package commands.system;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Clear {
+    private static final String os = System.getProperty("os.name");
+
+    public static void clear (ArrayList<String> args) {
+        try {
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
+            }
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
