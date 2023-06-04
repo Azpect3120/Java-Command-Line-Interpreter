f56b1f1a7ff7dcaa465659b0e12e9de508d08389
Azpect3120 <hhargreaves2006@gmail.com>
2023-06-03 19:53:09 -0700

    open command was migrated

--- src/main/java/commands/fileSystem/Open.java
+++ src/main/java/commands/fileSystem/Open.java
@@ -1,4 +1,31 @@
 package commands.fileSystem;
 
+import commands.Path;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Open {
+    private static void open (ArrayList<String> args) {
+        File targetPath;
+
+        if (args.isEmpty()) {
+            targetPath = new File(Path.getPath());
+        } else {
+            targetPath = new File(Path.getPath() + String.join(" ", args));
+        }
+
+        if (targetPath.exists()) {
+            try {
+                Desktop.getDesktop().open(targetPath);
+            } catch (IOException e) {
+                System.out.println("Unable to open file or directory");
+            }
+        } else {
+            System.out.println("File or directory was not found");
+        }
+    }
+
 }
