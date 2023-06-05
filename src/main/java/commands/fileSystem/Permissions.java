fd4134d39b0f34070f6edd0d02133d4fef778cff
Azpect3120 <hhargreaves2006@gmail.com>
2023-06-04 20:10:46 -0700

    permissions is still not working on USB drives. Compress command was scrapped as well :(

--- src/main/java/commands/fileSystem/Permissions.java
+++ src/main/java/commands/fileSystem/Permissions.java
@@ -4,11 +4,10 @@ import commands.CurrentPath;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.AclEntry;
-import java.nio.file.attribute.AclFileAttributeView;
-import java.nio.file.attribute.UserPrincipal;
+import java.nio.file.attribute.*;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Permissions {
     public static void permissions (ArrayList<String> args) {
@@ -23,30 +22,30 @@ public class Permissions {
             AclFileAttributeView aclView = Files.getFileAttributeView(Path.of(pathString), AclFileAttributeView.class);
             List<AclEntry> aclEntries = aclView.getAcl();
 
-            for (int i = 0; i < aclEntries.size(); i+=2) {
-                UserPrincipal principal1 = aclEntries.get(i).principal();
-                UserPrincipal principal2 = aclEntries.get(i+1).principal();
+            if (aclEntries.size() % 2 == 0) {
+                for (int i = 0; i < aclEntries.size(); i += 2) {
+                    UserPrincipal principal1 = aclEntries.get(i).principal();
+                    UserPrincipal principal2 = aclEntries.get(i + 1).principal();
 
-                if (principal1.equals(principal2)) {
-                    System.out.println("<user> " + principal1.getName().split("\\\\")[1]);
-                    System.out.println("<permissions> " + aclEntries.get(i).permissions());
-                    System.out.println("<type> " + aclEntries.get(i).type());
-                    System.out.println("<flags> " + aclEntries.get(i+1).flags());
+                    if (principal1.equals(principal2)) {
+                        System.out.println("<user> " + principal1.getName().split("\\\\")[1]);
+                        System.out.println("<permissions> " + aclEntries.get(i).permissions());
+                        System.out.println("<type> " + aclEntries.get(i).type());
+                        System.out.println("<flags> " + aclEntries.get(i + 1).flags());
+                        System.out.println("");
+                    }
+                }
+            } else {
+                for (AclEntry entry : aclEntries) {
+                    UserPrincipal principal = entry.principal();
+                    System.out.println("<user> " + principal.getName().split("\\\\")[1]);
+                    System.out.println("<permissions> " + entry.permissions());
+                    System.out.println("<type> " + entry.type());
+                    System.out.println("<flags> " + entry.flags());
                     System.out.println("");
+
                 }
             }
-
-
-//            for (AclEntry entry : aclEntries) {
-//                UserPrincipal principal = entry.principal();
-//                System.out.println(principal);
-//
-//                System.out.println("User: " + principal.getName());
-//                System.out.println("Permissions: " + entry.permissions());
-//                System.out.println("Type: " + entry.type());
-//                System.out.println("Flags: " + entry.flags());
-//                System.out.println("-----");
-//            }
         } catch (Exception e) {
             System.out.println("An error occurred while retrieving file permissions: " + e.getMessage());
         }
