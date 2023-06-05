6c34472c8a0c1962d484f13e24745dcacf5b897b
Azpect3120 <hhargreaves2006@gmail.com>
2023-06-04 19:19:38 -0700

    permissions command was added!

--- src/main/java/commands/fileSystem/Permissions.java
+++ src/main/java/commands/fileSystem/Permissions.java
@@ -23,14 +23,30 @@ public class Permissions {
             AclFileAttributeView aclView = Files.getFileAttributeView(Path.of(pathString), AclFileAttributeView.class);
             List<AclEntry> aclEntries = aclView.getAcl();
 
-            for (AclEntry entry : aclEntries) {
-                UserPrincipal principal = entry.principal();
-                System.out.println("User: " + principal.getName());
-                System.out.println("Permissions: " + entry.permissions());
-                System.out.println("Type: " + entry.type());
-                System.out.println("Flags: " + entry.flags());
-                System.out.println("-----");
+            for (int i = 0; i < aclEntries.size(); i+=2) {
+                UserPrincipal principal1 = aclEntries.get(i).principal();
+                UserPrincipal principal2 = aclEntries.get(i+1).principal();
+
+                if (principal1.equals(principal2)) {
+                    System.out.println("<user> " + principal1.getName().split("\\\\")[1]);
+                    System.out.println("<permissions> " + aclEntries.get(i).permissions());
+                    System.out.println("<type> " + aclEntries.get(i).type());
+                    System.out.println("<flags> " + aclEntries.get(i+1).flags());
+                    System.out.println("");
+                }
             }
+
+
+//            for (AclEntry entry : aclEntries) {
+//                UserPrincipal principal = entry.principal();
+//                System.out.println(principal);
+//
+//                System.out.println("User: " + principal.getName());
+//                System.out.println("Permissions: " + entry.permissions());
+//                System.out.println("Type: " + entry.type());
+//                System.out.println("Flags: " + entry.flags());
+//                System.out.println("-----");
+//            }
         } catch (Exception e) {
             System.out.println("An error occurred while retrieving file permissions: " + e.getMessage());
         }
